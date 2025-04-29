6019203d3701854fc9c5bee39f028f74dc3c003b
LuanAur <luanaugusto828@gmail.com>
2025-04-28 22:26:08 -0300

    Merge Latest Back + Pages
    Esse update leva para a main o backMarcos e as páginas estáticas.

---
+++ src/main/java/br/com/marcos/controller/FuncionarioController.java
@@ -0,0 +1,76 @@
+package br.com.marcos.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.marcos.dto.RequestFuncionario;
+import br.com.marcos.model.Funcionario;
+import br.com.marcos.service.FuncionarioService;
+import io.netty.util.internal.logging.Log4J2LoggerFactory;
+
+@RequestMapping(value = "/funcionario")
+
+@RestController
+public class FuncionarioController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(FuncionarioController.class);
+
+			
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
+	
+	@GetMapping
+	public ResponseEntity<List<Funcionario>> getAll(){
+		logger.debug("Iniciando requisicao Get para Funcionario!");
+		List<Funcionario> funcionarios = funcionarioService.list();
+		return ResponseEntity.ok(funcionarios);
+		
+	}
+	
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		logger.debug("Iniciando requisicao Delete Funcionario para o Id:"+id);
+		funcionarioService.delete(id);
+		return ResponseEntity.ok().build();
+		
+	}
+	
+	
+	@PutMapping
+	public ResponseEntity<Funcionario> update(@RequestBody RequestFuncionario dto , @RequestBody Long id){
+		logger.debug("Iniciando requisicao Update!"); 
+		logger.debug("Id do funcionario a ser atualizado:"+id);
+		logger.debug("Atualizando funcionario para novos dados:"+dto);
+		Funcionario newFuncionario = funcionarioService.updte(id, dto);
+		return ResponseEntity.ok(newFuncionario);
+		
+	}
+	
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Funcionario> post(@RequestBody RequestFuncionario dto ){
+		logger.debug("Iniciando requisicao Post para Funcionario!");
+		logger.debug("Inserindo novo Funcionario:"+dto);
+		Funcionario funcionario = funcionarioService.create(dto);
+		return ResponseEntity.ok(funcionario);
+		
+	}
+	
+	
+
+}
--- src/main/java/br/com/marcos/service/FuncionarioService.java
+++ src/main/java/br/com/marcos/service/FuncionarioService.java
@@ -1,8 +1,9 @@
 package br.com.marcos.service;
 
 import java.util.List;
-import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,11 @@ import br.com.marcos.repository.FuncionarioRepository;
 
 @Service
 public class FuncionarioService {
-	@Autowired
-	private FuncionarioRepository funcionarioRepository;
+
+@Autowired
+private FuncionarioRepository funcionarioRepository;
+	
+	private static final Logger logger = LoggerFactory.getLogger(FuncionarioService.class);
 	
 	
 	public List<Funcionario> list(){
@@ -24,24 +28,28 @@ public class FuncionarioService {
 	
 	
 	public Funcionario create(RequestFuncionario funcionarioDto) {
+		logger.debug("Inicializando create funcionario!");
 		Funcionario funcionario = new Funcionario(funcionarioDto);
 		funcionarioRepository.save(funcionario);
+		logger.debug("Funcionario salvo:"+ funcionario.toString());
 		return funcionario;
-		
 	}
 	
 	public Funcionario updte(Long id , RequestFuncionario updateFuncionario) {
+		logger.debug("Inicializando update funcionario!");
 		Funcionario funcionarioOld = funcionarioRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Funcionario nao encontrado"));
 		funcionarioOld.setNome(updateFuncionario.nome());
 		funcionarioOld.setAutenticacao(updateFuncionario.autenticacao());
 		funcionarioOld.setCargo(updateFuncionario.cargo());
 		funcionarioOld.setSalario(updateFuncionario.salario());
+		logger.debug("Funcionario atualizado:"+ funcionarioOld.toString());
 		return funcionarioOld;
 		
 	}
 	
 	public void delete(Long id) {
+		logger.debug("Deletando Funcionario de Id:"+id);
 		funcionarioRepository.deleteById(id);
 	}
-
+	
 }
