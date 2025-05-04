251253343d81332aa945dc45852cc2caa4c18bbd
Marcos <MarcosViniciusCM@outlook.com>
2025-05-04 11:25:13 -0300

    Add sistema de logs e logs salvo em memoria

--- src/main/java/br/com/marcos/service/FuncionarioService.java
+++ src/main/java/br/com/marcos/service/FuncionarioService.java
@@ -1,8 +1,9 @@
 package br.com.marcos.service;
 
 import java.util.List;
-import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,37 +11,44 @@ import br.com.marcos.dto.RequestFuncionario;
 import br.com.marcos.exceptionResponse.ResourceNotFoundException;
 import br.com.marcos.model.Funcionario;
 import br.com.marcos.repository.FuncionarioRepository;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 public class FuncionarioService {
 	@Autowired
 	private FuncionarioRepository funcionarioRepository;
 	
-	
-	public List<Funcionario> list(){
+    private static final Logger log = LoggerFactory.getLogger(FuncionarioService.class);
+
+
+	public List<Funcionario> list() {
 		List<Funcionario> list = funcionarioRepository.findAll();
 		return list;
 	}
-	
-	
+
 	public Funcionario create(RequestFuncionario funcionarioDto) {
+		log.info("Inicializando create funcionario!");
 		Funcionario funcionario = new Funcionario(funcionarioDto);
 		funcionarioRepository.save(funcionario);
+		log.debug("Funcionario salvo:" + funcionario.toString());
 		return funcionario;
-		
 	}
-	
-	public Funcionario updte(Long id , RequestFuncionario updateFuncionario) {
-		Funcionario funcionarioOld = funcionarioRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Funcionario nao encontrado"));
+
+	public Funcionario updte(Long id, RequestFuncionario updateFuncionario) {
+		log.debug("Inicializando update funcionario!");
+		Funcionario funcionarioOld = funcionarioRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Funcionario nao encontrado"));
 		funcionarioOld.setNome(updateFuncionario.nome());
 		funcionarioOld.setAutenticacao(updateFuncionario.autenticacao());
 		funcionarioOld.setCargo(updateFuncionario.cargo());
 		funcionarioOld.setSalario(updateFuncionario.salario());
+		log.debug("Funcionario atualizado:" + funcionarioOld.toString());
 		return funcionarioOld;
-		
+
 	}
-	
+
 	public void delete(Long id) {
+		log.debug("Deletando Funcionario de Id:" + id);
 		funcionarioRepository.deleteById(id);
 	}
 
