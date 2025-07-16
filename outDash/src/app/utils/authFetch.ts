1a520f77d022cc348377afcfee5141c11b14ee64
LuanAur <127259655+LuanAur@users.noreply.github.com>
2025-07-15 21:03:39 -0300

    Idiot Proofing

--- outDash/src/app/utils/authFetch.ts
+++ outDash/src/app/utils/authFetch.ts
@@ -14,3 +14,5 @@ export const authFetch = async (url: string, options: RequestInit = {}) => {
     credentials: "include", 
   });
 };
+
+//"cara eu acabe de provar que eu sei oque eu to fazendo, eu nao preciso do desktop" mal ele sabia, que não, ele não fazia ideia doq tava fazendo
